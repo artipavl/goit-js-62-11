0071127ffe32dd3fe82e7d35a45069a690baf22f
artipavl <40296816+artipavl@users.noreply.github.com>
2022-10-20 20:44:13 +0000

    Deploying to gh-pages from @ artipavl/goit-js-62-11@b587dcda24eff35d11b75b74331923bd77f589be 🚀

--- index.7837d67e.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault(),e.target.elements.searchQuery.value.trim()||console.log("pust")}));
-//# sourceMappingURL=index.7837d67e.js.map
--- index.818aa6c8.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-form").addEventListener("submit",(e=>{e.preventDefault();e.target.elements.searchQuery.value.trim()||console.log("pust")}));
-//# sourceMappingURL=index.818aa6c8.js.map
