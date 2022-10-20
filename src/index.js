0df06affdfd9d7cccb6c259176ec609e343fb3a0
artipavl <40296816+artipavl@users.noreply.github.com>
2022-10-20 22:03:30 +0300

    add submit form in js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('adsasadasd')
+const form = document.querySelector("#search-form")
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const searchQuery = event.target.elements.searchQuery.value.trim();
+
+    if (!searchQuery) {
+        console.log("pust")
+        return;
+    }
+
+    
+})
