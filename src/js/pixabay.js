4e1d94a28ee9a5dd494fc1b064e316cdb0df6fd8
artipavl <40296816+artipavl@users.noreply.github.com>
2022-10-24 09:42:00 +0300

    Update pixabay.js

--- src/js/pixabay.js
+++ src/js/pixabay.js
@@ -4,10 +4,15 @@ const API = 'https://pixabay.com/api/';
 const API_KEY = '30745008-d5532b40a5a7d9416df3fd4b0';
 
 export default class Pixabay {
-  #pageItems
+  #pageItems;
+  #pageTotal;
+  #pageСounter;
+
   constructor() {
     this.page = 1;
     this.#pageItems = 0;
+    this.#pageTotal = 0;
+    this.#pageСounter = 0;
   }
 
   async getToServer(name) {
@@ -19,31 +24,40 @@ export default class Pixabay {
     const fatchServerResponse = serverResponse[0];
     fatchServerResponse.hits.push(...serverResponse[1].hits);
 
-    this.upPages();
     if (fatchServerResponse.totalHits === 0) {
       throw Error(
         'Sorry, there are no images matching your search query. Please try again.'
       );
     }
 
+    this.upPages();
+    this.calculatePagesTotal(fatchServerResponse);
+    this.upPagesСounter();
     this.addpageItems(fatchServerResponse);
 
     return fatchServerResponse;
   }
 
-  resetPages() {
+  reset() {
     this.page = 1;
     this.#pageItems = 0;
+    this.#pageСounter = 0;
+    this.#pageTotal = 0;
   }
 
   upPages() {
     this.page += 2;
   }
+  upPagesСounter() {
+    this.#pageСounter += 1;
+  }
+  calculatePagesTotal(response) {
+    this.#pageTotal = Math.ceil(response.totalHits / 40);
+  }
 
   addpageItems(response) {
     this.#pageItems += Number(response.hits.length);
   }
-
 
   async axiosGet(name, page) {
     const response = await axios.get(
@@ -54,5 +68,13 @@ export default class Pixabay {
 
   get pageItems() {
     return this.#pageItems;
+  }
+
+  get pageTotal() {
+    return this.#pageTotal;
+  }
+
+  get pageСounter() {
+    return this.#pageСounter;
   }
 }
